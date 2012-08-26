949db8ef19930139b9cc246d91eff9d2e775de8b
Adam Michalik <adam@michalik.es>
2012-08-25 18:16:43 -0700

    more header reorganization

---
+++ crypto_device.h
@@ -0,0 +1,6 @@
+/* -*- mode: C; fill-column: 80; c-file-style: "linux"; indent-tabs-mode: t  -*- */
+
+struct cryptodev_t* get_cryptodev(void);
+
+int create_cryptoiface(void);
+void destroy_cryptoiface(void);
--- crypto_module.h
+++
@@ -1,3 +0,0 @@
-/* -*- mode: C; fill-column: 80; c-file-style: "linux"; indent-tabs-mode: t  -*- */
-
-static const unsigned int cryptodev_minor = 0;
